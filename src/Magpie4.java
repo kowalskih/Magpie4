a0ba3cbd5088d223ddeb29b9263168eff1cb9b67
kowalskih <kowalskih198@aol.com>
2014-10-24 11:39:17 -0400

    added stuff
    added stuff

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -53,9 +53,14 @@ public class Magpie4
 		{
 			response = transformIWantToStatement(statement);
 		}
-
+        // Responses which require transformations
+        else if (findKeyword(statement, "I want", 0) >= 0)
+        {
+            response = transformIWantStatement(statement);
+        }
 		else
-		{
+
+        {
 			// Look for a two word (you <something> me)
 			// pattern
 			int psn = findKeyword(statement, "you", 0);
@@ -93,9 +98,35 @@ public class Magpie4
 		int psn = findKeyword (statement, "I want to", 0);
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
+
+
+
 	}
 
-	
+    /**
+     * Take a statement with "I want to <something>." and transform it into
+     * "What would it mean to <something>?"
+     * @param statement the user statement, assumed to contain "I want to"
+     * @return the transformed statement
+     */
+    private String transformIWantStatement(String statement)
+    {
+        //  Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+        int psn = findKeyword (statement, "I want", 0);
+        String restOfStatement = statement.substring(psn + 7).trim();
+        return "Would you really be happy if you had " + restOfStatement + "?";
+
+
+
+    }
 	
 	/**
 	 * Take a statement with "you <something> me" and transform it into 
